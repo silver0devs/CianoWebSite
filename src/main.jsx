995500c39d79432d94d5dda1feb307cc7ef9c1e7
lrolando <silver0devs@gmail.com>
2024-05-12 17:12:15 -0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,14 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyB5LFOUTZdzIoE4nDv2ZOH4PL_H_Ru-S4A",
   authDomain: "silverwebsite-b768f.firebaseapp.com",
@@ -21,7 +15,6 @@ const firebaseConfig = {
   measurementId: "G-LVNY8BH057"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
